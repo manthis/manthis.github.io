0ef25ffddc9954cd77c8752fc1201fb9efa81be0
Maxime AUBURTIN <maxime@auburt.in>
2023-12-27 13:24:55 +0100

    Small modifications to the index files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
-
 // Main App component
 const App = () => {
     return <h1>Hello Max!</h1>;
